0f56263c4eda6b0d5e2996e051486d879aa8453c
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2023-03-03 03:44:35 -0800

    deploy to netlify

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from '@components/styles/Home.module.css'
+import Link from 'next/link'
 
 function Nav() {
   return (
     <nav className={styles.navbar}>
       <ul className={styles.ul}>
-        <li className={styles.li}><a className={styles.a} href="/">Videos</a></li>
-        <li className={styles.li}><a className={styles.a} href="/videoUploadPage">Upload</a></li>
+        <li className={styles.li}><Link className={styles.a} href="/">Videos</Link></li>
+        <li className={styles.li}><Link className={styles.a} href="/videoUploadPage">Upload</Link></li>
       </ul>
     </nav>
   )
